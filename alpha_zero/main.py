6166cf5c1c92f357a3b2ac32d109879746ac9ae6
AokiMasataka <4okimasataka@gmail.com>
2022-08-28 01:33:02 +0900

    :)

--- alpha_zero/main.py
+++ alpha_zero/main.py
@@ -1,11 +1,38 @@
+import numpy as np
 from game import Reversi
+from self_play import mcts_search, self_play, parallel_self_play
+
+
+class Model:
+    def __call__(self, state):
+        return np.random.random(), np.random.random(64)
+
+    def inference_state(self, state):
+        return np.random.random(), np.random.random(64)
+
+    def get_value(self, state):
+        return np.random.random()
 
 
 def main():
+    model = Model()
     game = Reversi(size=8)
-    print(game)
-    state = game.get_next_state(action=20)
-    print(state)
+
+    # play_history = self_play(game=Reversi, model=model, init_dict=dict(size=8), random_play=16)
+    # game.play_history_replay(play_history=play_history)
+
+    play_history = parallel_self_play(
+        model=model,
+        num_searchs=32,
+        num_games=4,
+        game=Reversi,
+        init_dict=dict(size=8),
+        random_play=8,
+        c_puct=1.0,
+        temperature=1.0
+    )
+
+    game.play_history_replay(play_history=play_history, n=10)
 
 
 if __name__ == '__main__':
