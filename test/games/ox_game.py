1df283f2de4a7175d35d044fd43bbe89b6d7f0a9
AokiMasataka <4okimasataka@gmail.com>
2023-05-01 20:25:57 +0900

    implement ox game tests

--- test/games/ox_game.py
+++ test/games/ox_game.py
@@ -6,10 +6,15 @@ from alpha_zero.games import OXGame
 def main():
     game = OXGame()
 
-    game = OXGame.action_functional(obj=game, action=0)
+    for _ in range(32):
+        legal_actions = game.get_legal_action()
+        game = game.action(legal_actions[0])
+        print(game)
+        if game.is_done():
+            break
+
     print(game)
-    game = OXGame.action_functional(obj=game, action=1)
-    print(game)
+    print(game.get_winner())
 
     
 
